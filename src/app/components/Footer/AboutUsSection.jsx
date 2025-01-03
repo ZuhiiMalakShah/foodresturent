52d5fb4290a7a69957c46f6c714fd9274cb8ce34
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-03 11:17:44 +0500

    Update AboutUsSection.jsx

--- src/app/components/Footer/AboutUsSection.jsx
+++ src/app/components/Footer/AboutUsSection.jsx
@@ -5,20 +5,18 @@ import Clock from "../assets/Clockwise.svg";
 const AboutUsSection = () => {
   return (
     <div className="col-lg-4">
-      <span className="fs-5 fw-bold">About us.</span>
+      <span className="fs-5 fw-bold">About Us</span>
       <p className="mt-4">
-        Corporate clients and leisure travelers have been relying on Groundlink
-        for dependable, safe, and professional chauffeured car service in major
-        cities across the world.
+        Our corporate clients and leisure travelers trust us for reliable, safe, and professional chauffeured car services in major cities around the world.
       </p>
       <div className="container d-flex pe-5">
-        <div className="text-center rounded yellowBg" style={{width: "5rem", height: "5rem"}}>
-          <Image src={Clock} alt="clock" className="img-fluid py-3" />
+        <div className="text-center rounded yellowBg" style={{ width: "5rem", height: "5rem" }}>
+          <Image src={Clock} alt="Clock Icon" className="img-fluid py-3" />
         </div>
         <div className="px-3">
-          <p className="fs-6 lh-1">Opening hours</p>
-          <p className="fs-6 lh-1">Mon - Sat(8.00 - 6.00)</p>
-          <span className="fs-6 lh-1">Sunday - Closed</span>
+          <p className="fs-6 lh-1">Opening Hours</p>
+          <p className="fs-6 lh-1">Monday - Saturday: 8:00 AM - 6:00 PM</p>
+          <span className="fs-6 lh-1">Sunday: Closed</span>
         </div>
       </div>
     </div>
