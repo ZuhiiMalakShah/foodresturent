9e35388c8465adaaf8c28bd38d0cabff12f29450
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-03 11:03:03 +0500

    Update MainCourse.jsx

--- src/app/components/MenuPageSections/MainCourse/MainCourse.jsx
+++ src/app/components/MenuPageSections/MainCourse/MainCourse.jsx
@@ -7,28 +7,28 @@ import Image from "next/image";
 const MainCourse = () => {
   const menuList = [
     {
-      title: "Optic Big Breakfast Combo Menu",
+      title: "Big Breakfast Combo",
       description: "Toasted French bread topped with romano, cheddar",
       calories: "560 CAL",
-      price: "$32",
+      price: "PKR 9,000",
     },
     {
-      title: "Cashew Chicken With Stir-Fry",
+      title: "Cashew Chicken Stir-Fry",
       description: "Gorgonzola, ricotta, mozzarella, taleggio",
       calories: "700 CAL",
-      price: "$43",
+      price: "PKR 12,000",
     },
     {
-      title: " Vegetables & Green Salad",
+      title: "Vegetable & Green Salad",
       description: "Ground cumin, avocados, peeled and cubed",
       calories: "1000 CAL",
-      price: "$14",
+      price: "PKR 4,000",
     },
     {
       title: "Spicy Vegan Potato Curry",
       description: "Spreadable cream cheese, crumbled blue cheese",
       calories: "560 CAL",
-      price: "$35",
+      price: "PKR 10,000",
     },
   ];
   return (
@@ -48,7 +48,7 @@ const MainCourse = () => {
           ))}
         </div>
         <div className="col-lg-5 mt-2">
-          <Image src={Burger} className="img-fluid" alt="Salmon" />
+          <Image src={Burger} className="img-fluid" alt="Burger" />
         </div>
       </div>
     </div>
