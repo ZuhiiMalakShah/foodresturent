1d4fe8d5616819ba0b67cc184ab6aac34ff01369
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-03 11:43:09 +0500

    Update BottomFooter.jsx

--- src/app/components/Footer/BottomFooter.jsx
+++ src/app/components/Footer/BottomFooter.jsx
@@ -13,8 +13,8 @@ const BottomFooter = () => {
         <div className="row">
           <div className="col-lg-6 d-flex align-items-center">
             <p className="p-2 my-2">
-              Copyright © 2022 by Hafsa Manzoor. All Rights Reserved. <br />
-              Owner: Hafsa Manzoor <br />
+              Copyright © 2022 by Aliya Manzoor. All Rights Reserved. <br />
+              Owner: Aliya Manzoor <br />
               Email: <a href="mailto:hafsamz212@gmail.com">hafsamz212@gmail.com</a>
             </p>
           </div>
